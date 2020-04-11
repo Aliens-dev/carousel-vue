044f918dd33a7ecd2398a5dc38ffcd70c2d62040
Aliens-Dev <merazganabil34@gmail.com>
2020-04-11 16:56:24 +0100

    1-using button to control

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="app">
+        <carousel class="carousel" >
+            <carousel-slide v-for="slide in slides" :key="slide" class="carousel-slider">
+                <img :src="slide" :alt="slide">
+            </carousel-slide>
+        </carousel>
+    </div>    
+</template>
+
+<script>
+import Carousel from './components/carousel/Carousel.vue';
+import CarouselSlide from './components/carousel/CarouselSlide';
+
+export default {
+    data(){
+        return {
+            slides: [
+                'https://picsum.photos/id/230/600/300',
+                'https://picsum.photos/id/231/600/300',
+                'https://picsum.photos/id/232/600/300',
+                'https://picsum.photos/id/233/600/300',
+                'https://picsum.photos/id/234/600/300',
+                'https://picsum.photos/id/235/600/300',
+                'https://picsum.photos/id/236/600/300',
+            ]
+        }
+    },
+    components : {
+        Carousel : Carousel,
+        CarouselSlide : CarouselSlide,
+    }
+}
+</script>
+
+<style>
+    .app {
+        display:flex;
+        justify-content: center;
+        
+    }
+    .carousel {
+        position:relative;
+        overflow: hidden;
+        width:800px;
+        height:500px;
+    }
+    .btn {
+        padding:5px 10px;
+        background-color:rgba(0,0,0,0.5);
+        border:1px solid transparent;
+        margin:5px 10px;
+        color:#FFF;
+        height:50px;
+        width:50px;
+        position:absolute;
+        margin-top:-25px;
+        z-index:2;
+    }
+    .btn:hover {
+        cursor: pointer;
+    }
+    .btn:focus{
+        outline:none;
+    }
+    .btn-next {
+        top:50%;
+        right:0;
+    }
+    .btn-prev {
+        top:50%;
+        left:0;
+    }
+    .carousel-slider {
+        z-index:1;
+        position:absolute;
+        top:0;
+        left:0;
+        bottom:0;
+        right:0;
+    }
+    .carousel-slider img {
+        width:100%;
+        height:100%;
+    }
+
+</style>
---
+++ src/components/carousel/Carousel.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <slot></slot>
+        <button @click.prevent="next" class="btn btn-next"><i class="fa fa-angle-right"></i></button>
+        <button @click.prevent="prev" class="btn btn-prev"><i class="fa fa-angle-left"></i></button>
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            index : 0,
+            slides : [],
+            slideDirection: '',
+        }
+    },
+    computed: {
+        slidesLength() {
+            return this.slides.length;
+        }
+    },
+    mounted(){
+        this.slides = this.$children;
+        this.slides.map( (slide,index) => {
+            slide.index = index;
+        });
+    },
+    methods: {
+        next(){
+            this.index++;
+            if(this.index >= this.slidesLength){
+                this.index = 0;
+            }
+            this.slideDirection = 'slide-right';
+        },
+        prev(){
+            this.index--;
+            if(this.index < 0){
+                this.index = this.slidesLength - 1;
+            }
+             this.slideDirection = 'slide-left';
+        },
+    }
+}
+</script>
+<style>
+    
+</style>
